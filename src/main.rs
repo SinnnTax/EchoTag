068a0f6350b3f9a616912e4163b93543ed09f5f1
Ahura Majidi <78syntax@gmail.com>
2026-07-09 15:38:47 +0330

    chore: experiment with std::process::Command for external tools

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let a = Command::new("D:\\rust.etc\\EchoTag\\a.bat").output().unwrap();
+    assert_eq!("hello\r\n", String::from_utf8(a.stdout).unwrap());
 }
