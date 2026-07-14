3bf3a06d878505dafe0dedba68d2ce1b97b524a3
Ahura Majidi <78syntax@gmail.com>
2026-07-14 09:43:06 +0330

    chore: experiment with yt-dlp arguments for audio download

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
 use std::process::Command;
 
 fn main() {
-    let a = Command::new("D:\\rust.etc\\EchoTag\\a.bat").output().unwrap();
-    assert_eq!("hello\r\n", String::from_utf8(a.stdout).unwrap());
+    let mut ytdlp = Command::new("yt-dlp");
+    let ytdlp = ytdlp
+        .args([
+            "-x",
+            "--audio-format",
+            "mp3",
+            "--audio-quality",
+            "0",
+            "-o",
+            "%(title)s.%(ext)s",
+            "-q",
+            "--force-ipv4",
+            "--cookies",
+            "D:\\rust.etc\\EchoTag\\cookies.txt",
+            "--print",
+            "%(channel)s",
+            "--print",
+            "%(title)s",
+            "--print",
+            "after_move:filepath",
+            "https://youtu.be/eZtlb9eegj0?si=gZ6SI0-qecfiTJLY",
+        ])
+        .output()
+        .unwrap();
+    println!("{}", String::from_utf8(ytdlp.stdout).unwrap());
 }
