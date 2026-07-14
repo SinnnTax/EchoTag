95c3dceddca24c5899b3d7f3d6474c2a4f2e3f15
Ahura Majidi <78syntax@gmail.com>
2026-07-14 17:19:10 +0330

    feat: add youtube download function with optional cookies

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,45 @@
 use std::process::Command;
+use std::process::Output;
+use std::io::Result;
+
+fn download_youtube(url: String, cookies: Option<String>) -> Result<Output> {
+    let mut ytdlp = Command::new("yt-dlp");
+
+    ytdlp.args([
+        "-x",
+        "--audio-format",
+        "mp3",
+        "--audio-quality",
+        "0",
+        "-o",
+        "%(title)s.%(ext)s",
+        "-q",
+        "--force-ipv4",
+        "--print",
+        "%(channel)s",
+        "--print",
+        "%(title)s",
+        "--print",
+        "after_move:filepath",
+    ]);
+
+    if let Some(cookie) = cookies {
+        ytdlp.args(["--cookies", &cookie]);
+    }
+
+    ytdlp.arg(url);
+
+    ytdlp.output()
+}
 
 fn main() {
-    let mut ytdlp = Command::new("yt-dlp");
-    let ytdlp = ytdlp
-        .args([
-            "-x",
-            "--audio-format",
-            "mp3",
-            "--audio-quality",
-            "0",
-            "-o",
-            "%(title)s.%(ext)s",
-            "-q",
-            "--force-ipv4",
-            "--cookies",
-            "D:\\rust.etc\\EchoTag\\cookies.txt",
-            "--print",
-            "%(channel)s",
-            "--print",
-            "%(title)s",
-            "--print",
-            "after_move:filepath",
-            "https://youtu.be/eZtlb9eegj0?si=gZ6SI0-qecfiTJLY",
-        ])
-        .output()
-        .unwrap();
-    println!("{}", String::from_utf8(ytdlp.stdout).unwrap());
+    let result = download_youtube(
+        "https://youtu.be/eZtlb9eegj0".to_string(),
+        Some("D:\\rust.etc\\EchoTag\\cookies.txt".to_string())
+    );
+
+    match result {
+        Ok(output) => println!("{}", String::from_utf8_lossy(&output.stdout)),
+        Err(e) => eprintln!("Error: {}", e),
+    }
 }
