b0e1ee212bf43f8fec2778840ff07c375f290c1c
Ahura Majidi <78syntax@gmail.com>
2026-07-23 18:11:03 +0330

    chore(cache): experiment with axum shared state

--- src/bin/cache_server.rs
+++ src/bin/cache_server.rs
@@ -1,8 +1,18 @@
-use axum::{ routing::get, Router, extract::Path };
+use axum::{ routing::get, Router, extract::{ Path, State } };
+
+#[derive(Clone)]
+struct AppState {
+    name: String,
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/health", get(health_check)).route("/cache/{id}", get(get_id));
+    let state = AppState { name: "EchoTag Cache Server".to_string() };
+
+    let app = Router::new()
+        .route("/health", get(health_check))
+        .route("/cache/{id}", get(get_id))
+        .with_state(state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
@@ -12,6 +22,6 @@ async fn health_check() -> &'static str {
     "connected!"
 }
 
-async fn get_id(Path(id): Path<u32>) -> String {
-    format!("id is {id}")
+async fn get_id(State(state): State<AppState>, Path(id): Path<u32>) -> String {
+    format!("message from {}: id is {id}", state.name)
 }
