efcd5a17fd9b58bb74c89bc17f38e5a943afeb27
Ahura Majidi <78syntax@gmail.com>
2026-08-03 16:30:37 +0330

    feat(cache): expose server to external connections

--- src/bin/cache_server.rs
+++ src/bin/cache_server.rs
@@ -41,7 +41,7 @@ async fn main() {
         .layer(DefaultBodyLimit::disable())
         .with_state(state);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
