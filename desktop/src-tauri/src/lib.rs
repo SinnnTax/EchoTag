d52b90bad6eabb4681e49ca2765e1f69f3505cce
Ahura Majidi <78syntax@gmail.com>
2026-08-07 02:57:38 +0330

    feat: initialize tauri desktop app

---
+++ desktop/src-tauri/src/lib.rs
@@ -0,0 +1,20 @@
+// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[tauri::command]
+fn get_project_info() -> String {
+    format!("EchoTag Desktop - Built with Rust and Tauri!")
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder
+        ::default()
+        .plugin(tauri_plugin_opener::init())
+        .invoke_handler(tauri::generate_handler![greet, get_project_info])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ desktop/src/main.js
@@ -0,0 +1,31 @@
+const { invoke } = window.__TAURI__.core;
+
+let greetInputEl;
+let greetMsgEl;
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+async function project_info() {
+  greetMsgEl.textContent = await invoke("get_project_info", {});
+}
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   greetInputEl = document.querySelector("#greet-input");
+//   greetMsgEl = document.querySelector("#greet-msg");
+//   document.querySelector("#greet-form").addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     greet();
+//   });
+// });
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document.querySelector("#greet-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    project_info();
+  });
+});
